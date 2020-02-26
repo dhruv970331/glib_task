f53c0b6313313c48a861b23506750cc1d84f831d
dhruv970331 <dhruveshmalhotra186@gmail.com>
2020-02-26 11:07:51 +0530

    added static settings in base urls

--- glib_task/urls.py
+++ glib_task/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.views import View
 from django.shortcuts import render,redirect,reverse
+from django.conf import settings
 
 class HomeView(View):
     def get(self,request):
@@ -35,5 +36,5 @@ if settings.DEBUG:
      from django.conf.urls.static import static
      urlpatterns += static(settings.STATIC_URL,
                            document_root=settings.STATIC_ROOT)
-     urlpatterns += static(settings.MEDIA_URL,
-                           document_root=settings.MEDIA_ROOT)
+    #  urlpatterns += static(settings.MEDIA_URL,
+    #                        document_root=settings.MEDIA_ROOT)
