1103baecf4757f8cd02a6ac4a6143d543637de8d
dhruv970331 <dhruveshmalhotra186@gmail.com>
2020-02-26 11:05:49 +0530

    added static settings in base urls

--- glib_task/urls.py
+++ glib_task/urls.py
@@ -30,3 +30,10 @@ urlpatterns = [
     path("accounts/",include("accounts.urls")),
     path("snippets/",include("snippets.urls"))
 ]
+if settings.DEBUG:
+     # test mode
+     from django.conf.urls.static import static
+     urlpatterns += static(settings.STATIC_URL,
+                           document_root=settings.STATIC_ROOT)
+     urlpatterns += static(settings.MEDIA_URL,
+                           document_root=settings.MEDIA_ROOT)
